361fb8d8659039fda7ac8c719d98e317515ffc99
MikaQ56 <mickaelquintin@me.com>
2018-06-04 22:17:12 +0200

    'Nain' class added

---
+++ SwiftGame/Nain.swift
@@ -0,0 +1,20 @@
+//
+//  Nain.swift
+//  SwiftGame
+//
+//  Created by Mickael on 04/06/2018.
+//  Copyright © 2018 Mickael. All rights reserved.
+//
+
+import Foundation
+
+// Create character of type Nain
+class Nain: Character {
+    
+    override init(name: String){
+        super.init(name: name)
+        strength = 0
+        health = helmet["Wooden"]!
+        type = "Nain"
+    }
+}
