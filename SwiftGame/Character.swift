6f292c99ea37509082ccf8ac664ceed95ef7e212
MikaQ56 <mickaelquintin@me.com>
2018-06-04 22:10:35 +0200

    Properties & greeting function added to 'Character' class

--- SwiftGame/Character.swift
+++ SwiftGame/Character.swift
@@ -8,6 +8,37 @@
 
 import Foundation
 
+// Create 'Character' class to manage characters in the game
 class Character {
     
+    // Character's name
+    var name: String
+    
+    // Character's health points
+    var health = 0
+    
+    // Character's strength points
+    var strength = 0
+    
+    // Character's type
+    var type = String()
+    
+    // Character's Helmet
+    var helmet = [
+        "Wooden": 50,
+        "Iron": 100,
+        "Diamond": 150
+    ]
+    
+    // Initialize Character
+    init(name: String){
+        
+        self.name = name
+    }
+    
+    // Character's greeting
+    func introduction(){
+        
+        print("\(name), est un \(type), il a une force de \(strength) et \(health) points de vie\n")
+    }
 }
