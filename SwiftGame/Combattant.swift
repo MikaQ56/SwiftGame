16d7dc471ba32880d1a1c9708fd26f902be95680
MikaQ56 <mickaelquintin@me.com>
2018-06-04 22:13:21 +0200

    'Combattant' class added

---
+++ SwiftGame/Combattant.swift
@@ -0,0 +1,20 @@
+//
+//  Combattant.swift
+//  SwiftGame
+//
+//  Created by Mickael on 04/06/2018.
+//  Copyright © 2018 Mickael. All rights reserved.
+//
+
+import Foundation
+
+// Create character of type Combattant
+class Combattant: Character {
+    
+    override init(name: String){
+        super.init(name: name)
+        strength = 10
+        health = helmet["Iron"]!
+        type = "Combattant"
+    }
+}
